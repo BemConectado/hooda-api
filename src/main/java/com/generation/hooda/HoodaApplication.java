b25175085d25c32ad962d1337967e5f17d3a355d
Alexander Pereira <xandi9731@gmail.com>
2024-07-05 15:24:30 -0300

    Config: Iniciando projeto

---
+++ src/main/java/com/generation/hooda/HoodaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.hooda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoodaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/hooda/HoodaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.hooda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoodaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
