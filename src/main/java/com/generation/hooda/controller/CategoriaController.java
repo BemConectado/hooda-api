1e1ccbee4997730308c4c6fcc585ea0eaa0efaa3
Wil220 <wilker.silva0714@gmail.com>
2024-07-11 14:08:56 -0300

    Feat: adicinando metodos Byid e metodo post

--- src/main/java/com/generation/hooda/controller/CategoriaController.java
+++ src/main/java/com/generation/hooda/controller/CategoriaController.java
@@ -2,9 +2,16 @@ package com.generation.hooda.controller;
 
 import com.generation.hooda.model.Categoria;
 import com.generation.hooda.repository.CategoriaRepository;
+
+import jakarta.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +27,18 @@ public class CategoriaController {
     @GetMapping
     ResponseEntity<List<Categoria>> findAll(){
         return ResponseEntity.ok(categoriaRepository.findAll());
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Categoria> getById (@PathVariable Long id){
+    	return categoriaRepository.findById(id)
+    			.map(resposta -> ResponseEntity.ok(resposta))
+    			.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+    
+    @PostMapping
+    public ResponseEntity<Categoria> post(@Valid @RequestBody Categoria categoria){
+    	return ResponseEntity.status(HttpStatus.CREATED)
+    			.body(categoriaRepository.save(categoria));
     }
 }
