898c0d2f5445dca1dd2ece92a903f140ed1ac9ed
Alexander Pereira <xandi9731@gmail.com>
2024-07-12 14:33:35 -0300

    Feat: Implementação do Post, Put e Delete da classe Produto

--- src/main/java/com/generation/hooda/controller/ProdutoController.java
+++ src/main/java/com/generation/hooda/controller/ProdutoController.java
@@ -2,17 +2,16 @@ package com.generation.hooda.controller;
 
 import java.util.List;
 
+import com.generation.hooda.repository.CategoriaRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.generation.hooda.model.Produto;
 import com.generation.hooda.repository.ProdutoRepository;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/produtos")
@@ -21,6 +20,10 @@ public class ProdutoController {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
 	
 	@GetMapping
 	public ResponseEntity<List<Produto>> getAll(){
@@ -32,5 +35,37 @@ public class ProdutoController {
 		return produtoRepository.findById(id)
 				.map(resposta -> ResponseEntity.ok(resposta))
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Produto>> getByNome(@PathVariable String nome){
+		return ResponseEntity.ok(produtoRepository.findAllByNomeContainingIgnoreCase(nome));
+	}
+
+	@PostMapping
+	public ResponseEntity<Produto> create(@RequestBody Produto produto){
+		if(categoriaRepository.existsById(produto.getCategoria().getId())){
+			return ResponseEntity.status(HttpStatus.CREATED).body(produtoRepository.save(produto));
+		}
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Categoria inválida", null);
+	}
+
+	@PutMapping
+	public ResponseEntity<Produto> update(@Valid @RequestBody Produto produto){
+		if (produtoRepository.existsById(produto.getId())){
+			if(categoriaRepository.existsById(produto.getCategoria().getId())){
+				return ResponseEntity.ok().body(produtoRepository.save(produto));
+			}
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Categoria inválida", null);
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id){
+		if (!produtoRepository.existsById(id)){
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+		produtoRepository.deleteById(id);
 	}
 }
--- src/main/java/com/generation/hooda/repository/ProdutoRepository.java
+++ src/main/java/com/generation/hooda/repository/ProdutoRepository.java
@@ -3,8 +3,12 @@ package com.generation.hooda.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.hooda.model.Produto;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+    public List<Produto> findAllByNomeContainingIgnoreCase(@Param("nome")String nome);
 
 
 }
