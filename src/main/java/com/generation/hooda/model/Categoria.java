63b7357abffb443b866e09b51bc9df03a4f6827e
GabrielSantanaDaSilva <topsstdeq@gmail.com>
2024-07-12 13:14:45 -0300

    Feat: implementando a classe model e o relacionamento model categoria

--- src/main/java/com/generation/hooda/model/Categoria.java
+++ src/main/java/com/generation/hooda/model/Categoria.java
@@ -1,9 +1,16 @@
 package com.generation.hooda.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -23,6 +30,10 @@ public class Categoria {
 	@NotBlank(message = "O Atributo descrição é obrigatório")
 	@Size(min = 10, max = 500, message = "O atributo descrição deve conter no minimo 10 e no maximo 500 caracteres")
 	private String descricao;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("tema")
+	private List<Produto> produto;
 
 	public Long getId() {
 		return id;
@@ -47,5 +58,15 @@ public class Categoria {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+	
+	
 
 }
