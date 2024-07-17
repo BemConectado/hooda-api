c1bf5ce8d462617721225725be22560d03578496
Alexander Pereira <xandi9731@gmail.com>
2024-07-17 15:02:04 -0300

    Feat: Implementação do userDetailsService, JwtService e JwtAuthFilter

--- src/main/java/com/generation/hooda/service/UsuarioService.java
+++ src/main/java/com/generation/hooda/service/UsuarioService.java
@@ -2,18 +2,21 @@ package com.generation.hooda.service;
 
 import java.util.Optional;
 
+import com.generation.hooda.security.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.hooda.model.Usuario;
 import com.generation.hooda.model.UsuarioLogin;
 import com.generation.hooda.repository.UsuarioRepository;
 
+@Service
 public class UsuarioService {
 
 	@Autowired
@@ -26,7 +29,7 @@ public class UsuarioService {
 	private AuthenticationManager authenticationManager;
 
 	private String criptografarSenha(String senha) {
-		BCryptPasswordEncoder encoder = new BCrystPasswordEncoder();
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		return encoder.encode(senha);
 	}
 
@@ -51,7 +54,7 @@ public class UsuarioService {
 			if ((buscaUsuario.isPresent()) && (buscaUsuario.get().getId() != usuario.getId()))
 				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário já existe!", null);
 			
-			usuario.setSenha((criptografarSenha(usuario.getSenha()));
+			usuario.setSenha(criptografarSenha(usuario.getSenha()));
 			
 			return Optional.ofNullable(usuarioRepository.save(usuario));
 		}
